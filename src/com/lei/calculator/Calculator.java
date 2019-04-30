db42d9efa0d30d3a12111e1ddbabc7594fc805c9
Lei Xu <qsxulei@gmail.com>
2019-04-30 13:03:23 -0600

    4th add all the code. actionlinstern has problem.

--- src/com/lei/calculator/Calculator.java
+++ src/com/lei/calculator/Calculator.java
@@ -3,30 +3,35 @@ package com.lei.calculator;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.math.BigDecimal;
 import java.util.Vector;
 import java.awt.event.KeyEvent;
 
 public class Calculator {
 
-    //Create strings to record two input numbers
+    //Create strings to record two input numbers, default as 0 for stable of program.
     String str1 = "0";
     String str2 = "0";
 
-    //Create singal recorder, default as +
+    //Create signal recorder, default as +
     String signal = "+";
 
     //Create result string to record the result
     String result = "";
 
-    /* Set status of switches. k1 as input direction, k2 as signal number,
-    * k3 and k4 indicate str1 and str2 to be clear or not, k5 indicates dot input or not.  */
+    //k1 indicate input str1 or str2.
     int k1 = 1;
+    //k2 record number of input signal. k2>1 indicate a calculation with multiple signals.
     int k2 = 1;
+    //k3 indicate str1 can be clear to be 0 or not. if k3=1, then str1 can be clear to be 0.
     int k3 = 1;
+    //k4 indicate str2 can be clear to be 0 or not. if k4=1, then str2 can be clear to be 0.
     int k4 = 1;
+    //if k5=1, then float number will be record.
     int k5 = 1;
 
-    //Store button status
+    //Store button status, to record if the signals are clicked contiguously.
     JButton store;
 
     @SuppressWarnings(("rawtypes"))
@@ -65,7 +70,9 @@ public class Calculator {
     //Constructor of Calculator
     public Calculator(){
 
+        //Set keyboard with button; ie. can press the corresponding key in keyboard instead of click it.
         button0.setMnemonic(KeyEvent.VK_0);
+        //TODO set other keyboard equal
 
         //Text align to right
         resultTextField.setHorizontalAlignment((JTextField.RIGHT));
@@ -76,6 +83,7 @@ public class Calculator {
         //Set pan1 as 4*4 matrix, with 5 pixel to the edge
         pan1.setLayout(new GridLayout(4, 4, 5, 5));
 
+        //Add UI parts to the panel
         pan1.add(button7);
         pan1.add(button8);
         pan1.add(button9);
@@ -102,7 +110,7 @@ public class Calculator {
         pan2.add(resultTextField, BorderLayout.WEST);
         pan2.add(clearButton, BorderLayout.EAST);
 
-        //Set windows position
+        //Set windows position in the screen
         frame.setLocation(300, 200);
 
         frame.setResizable(false);
@@ -113,11 +121,14 @@ public class Calculator {
         frame.pack();
         frame.setVisible(true);
 
-        //Listen to keyboard input
-        class Listener implements ActionListener {
-            @SuppressWarnings("unchecked")
+        //Action listener to the input
+        //Listen to keyboard input, for numbers
+        class ListenerNumber implements ActionListener {
+           @SuppressWarnings("unchecked")
+
+            @Override
             public void actionPerformed(ActiveEvent e) {
-                String ss = ((JButton) e.getSource()).getText();
+                String ss = ((JButton)(e.getSource())).getText();
                 store = (JButton) e.getSource();
                 vt.add(store);
 
@@ -125,7 +136,7 @@ public class Calculator {
                     if (k3 == 1) {
                         str1 = "";
 
-
+                        //Reset k5.
                         k5 =1;
                     }
                     str1 = str1+ss;
@@ -148,8 +159,10 @@ public class Calculator {
             }
         }
 
+        //Listen to the input, for signal.
         class ListenerSignal implements ActionListener{
             //Operation singal
+            @SuppressWarnings("unchecked")
             public void actionPerformed(ActiveEvent e) {
                 String ss2 = ((JButton) e.getSource()).getText();
                 store = (JButton) e.getSource();
@@ -157,9 +170,13 @@ public class Calculator {
                 vt.add(store);
 
                 if (k2 == 1) {
+                    /* if k1==1, then input number is stored in str1;
+                    *if k1==2, then input number is stored in str2. */
                     k1 = 2;
                     k5 =1;
                     signal = ss2;
+                    //update signal input numbers.
+                    k2++;
                 }
 
                 else {
@@ -177,19 +194,188 @@ public class Calculator {
                         signal = ss2;
                     }
 
-                    k2 =k2+1;
+                    k2++;
                 }
 
             }
         }
 
+        //Logcial for clear button
         class ListenerClear implements ActionListener{
-            
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActiveEvent e) {
+                store = (JButton) e.getSource();
+                vt.add(store);
+                k5=1;
+                k2=1;
+                k1=1;
+                k3=1;
+                k4=1;
+                str1="0";
+                str2="0";
+                signal="";
+                result="";
+                resultTextField.setText(result);
+                vt.clear();
+            }
+
         }
 
+        //"=" event
+        class ListenerCal implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActiveEvent e) {
+                store = (JButton) e.getSource;
+                vt.add(store);
+                cal();
 
+                //reset all k values.
+                k1=1;
+                k2=1;
+                k3=1;
+                k4=1;
 
+                str1 = result;
+            }
+        }
 
+        //"." event
+        class ListenerFloat implements ActionListener {
+            @SuppressWarnings("unchecked")
+            public void actionPerformed(ActiveEvent e) {
+                store = (JButton) e.getSource();
+                vt.add(store);
+
+                if (k5 == 1) {
+                    String ss2 = ((JButton) e.getSource()).getText();
+
+                    if (k1 == 1) {
+                        if (k3 == 1) {
+                            str1 = "";
+
+                            k5 = 1;
+                        }
+                        str1 = str1 + ss2;
+
+                        k3++;
+
+                        resultTextField.setText(str1);
+                    } else if (k1 == 2) {
+                        if (k4 == 1) {
+                            str2 = "";
+                            k5 = 1;
+                        }
+                        str2 = str2 + ss2;
+
+                        k4++;
+
+                        resultTextField.setText(str2);
+                    }
+                }
+                k5++;
+            }
+        }
+
+        //Regist all listeners
+
+        ListenerCal LCal = new ListenerCal();
+
+        //Number key
+        ListenerNumber LNum = new ListenerNumber();
+        ListenerSignal LSignal = new ListenerSignal();
+        ListenerClear LClear = new ListenerClear();
+        ListenerFloat LFloat = new ListenerFloat();
+
+        button7.addActionListener(LNum);
+        button8.addActionListener(LNum);
+        button9.addActionListener(LNum);
+        buttonDive.addActionListener(LSignal);
+        button4.addActionListener(LNum);
+        button5.addActionListener(LNum);
+        button6.addActionListener(LNum);
+        buttonMulti.addActionListener(LSignal);
+        button1.addActionListener(LNum);
+        button2.addActionListener(LNum);
+        button3.addActionListener(LNum);
+        buttonMinus.addActionListener(LSignal);
+        button0.addActionListener(LNum);
+        buttonDot.addActionListener(LFloat);
+        buttonCal.addActionListener(LCal);
+        buttonPlus.addActionListener(LSignal);
+        clearButton.addActionListener(LClear);
+
+        //Close program windows
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowAdapter e) {
+                System.exit(0);
+            }
+        });
     }
 
+    //Calculation process
+    public void cal() {
+        //first number
+        double a2;
+        //second number
+        double b2;
+        //Operation signal
+        String c = signal;
+        //Operation result
+        double result2=0;
+
+        if (c.equals("")) {
+            resultTextField.setText("Please input operator!");
+        }
+        else {
+            //Float number
+            if (str1.equals("."))
+                str1 = "0.0";
+            if (str2.equals("."))
+                str2 = "0.0";
+
+            a2 = Double.valueOf(str1).doubleValue();
+            b2 = Double.valueOf(str2).doubleValue();
+
+            switch (c) {
+                case "+":
+                    result2 = a2+b2;
+                    break;
+                case "-":
+                    result2 = a2-b2;
+                    break;
+                case "*":
+                    BigDecimal m1 = new BigDecimal(Double.toString(a2));
+                    BigDecimal m2 = new BigDecimal(Double.toString(b2));
+
+                    result2 = m1.multiply(m2).doubleValue();
+                    break;
+                case "/":
+                    if (b2==0)
+                        result2 = 0;
+                    else
+                        result2 = a2 / b2;
+                    break;
+                default:
+                    resultTextField.setText("Wrong input!");
+                    break;
+            }
+
+            result = ((new Double(result2)).toString());
+
+            resultTextField.setText(result);
+        }
+    }
+
+    @SuppressWarnings("unused")
+
+    public static void main(String[] args){
+        //Set the display type
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Calculator cal = new Calculator();
+    }
 }
